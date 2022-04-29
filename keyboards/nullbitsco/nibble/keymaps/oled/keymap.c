c71de85db92ab06fbf819e11132e3df2995c572a
Dalgona <me@dalgona.dev>
2022-04-29 17:56:15 +0900

    Rotate underglow LED hue when knob is rotated with Fn key pressed

--- keyboards/nullbitsco/nibble/keymaps/oled/keymap.c
+++ keyboards/nullbitsco/nibble/keymaps/oled/keymap.c
@@ -124,10 +124,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (clockwise) {
-        tap_code(KC_VOLU);
+    if (IS_LAYER_ON(_FN)) {
+        if (clockwise) {
+            rgblight_increase_hue();
+        } else {
+            rgblight_decrease_hue();
+        }
     } else {
-        tap_code(KC_VOLD);
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
     }
     return true;
 }
